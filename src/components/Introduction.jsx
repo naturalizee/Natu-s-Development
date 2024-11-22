bdca559bcf71b50437447b4e349e2e7d6959c178
naturalizee <alizee.dereppe@gmail.com>
2024-11-22 13:34:00 +0100

    Editing the introductory sentence

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,10 +3,10 @@ import '../styles/introduction.scss'
 
 export function Introduction({ language }) {
     return (
-            <section className="introduction">
-                <h1>
-                    {language === 'fr' ? `Des sites dévelopés aujourd'hui, pensés pour demain.` : `Websites developed today, designed for tomorrow.`}
-                </h1>
-            </section>
+        <section className="introduction">
+            <h1>
+                {language === 'fr' ? `Des sites développés aujourd'hui, pensés pour demain.` : `Websites developed today, designed for tomorrow.`}
+            </h1>
+        </section>
     );
 }
